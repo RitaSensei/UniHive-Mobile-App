7a1148ece2ddced8dec1c7a12feb2543ad5c1047
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-04-10 12:25:37 +0000

    added login screen

--- android/app/src/main/java/com/biog/unihiveandroid/authentication/LoginActivity.java
+++ android/app/src/main/java/com/biog/unihiveandroid/authentication/LoginActivity.java
@@ -1,4 +1,15 @@
 package com.biog.unihiveandroid.authentication;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.biog.unihiveandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page    );
+    }
 }
